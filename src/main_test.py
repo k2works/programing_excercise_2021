64703a685346531c27bad39fe71427d9c1859d4a
k2works <kakimomokkuri@gmail.com>
2021-10-05 16:45:33 +0900

    test: 三角測量

--- src/main_test.py
+++ src/main_test.py
@@ -7,7 +7,12 @@ class Money:
         self.currency = currency
 
     def __str__(self) -> str:
-        return f'¥{self.amount}'
+        if self.currency == 'JPY':
+            return f'¥{self.amount}'
+        elif self.currency == 'USD':
+            return f'${self.amount}'
+        else:
+            return f'{self.amount}{self.currency}'
 
 class TestMoney(unittest.TestCase):
     def test_金額を表示する(self):
@@ -15,5 +20,9 @@ class TestMoney(unittest.TestCase):
         一万円 = Money(10000, 'JPY')
         self.assertEqual(str(千円), '¥1000')
         self.assertEqual(str(一万円), '¥10000')
+
+    def test_外貨金額を表示する(self):
+        千ドル = Money(1000, 'USD')
+        self.assertEqual(str(千ドル), '$1000')
 
 unittest.main(argv=[''], verbosity=2, exit=False)
