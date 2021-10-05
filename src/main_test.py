9f3f077ee1ef0d1bddea5be4732edf4f3a524122
k2works <kakimomokkuri@gmail.com>
2021-10-05 16:47:07 +0900

    refactor: リファクタリング

--- src/main_test.py
+++ src/main_test.py
@@ -15,14 +15,16 @@ class Money:
             return f'{self.amount}{self.currency}'
 
 class TestMoney(unittest.TestCase):
+    def setUp(self) -> None:
+        self.千円 = Money(1000, 'JPY')
+        self.一万円 = Money(10000, 'JPY')
+        self.千ドル = Money(1000, 'USD')
+
     def test_金額を表示する(self):
-        千円 = Money(1000, 'JPY')
-        一万円 = Money(10000, 'JPY')
-        self.assertEqual(str(千円), '¥1000')
-        self.assertEqual(str(一万円), '¥10000')
+        self.assertEqual(str(self.千円), '¥1000')
+        self.assertEqual(str(self.一万円), '¥10000')
 
     def test_外貨金額を表示する(self):
-        千ドル = Money(1000, 'USD')
-        self.assertEqual(str(千ドル), '$1000')
+        self.assertEqual(str(self.千ドル), '$1000')
 
 unittest.main(argv=[''], verbosity=2, exit=False)
