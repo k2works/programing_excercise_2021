b3fcc7a11226a2a0ecd42846f7a00a11b4f7e072
k2works <kakimomokkuri@gmail.com>
2021-10-05 16:43:18 +0900

    test: 三角測量

--- src/main_test.py
+++ src/main_test.py
@@ -3,14 +3,17 @@ import unittest
 
 class Money:
     def __init__(self, amount, currency) -> None:
-        pass
+        self.amount = amount
+        self.currency = currency
 
     def __str__(self) -> str:
-        return '¥1000'
+        return f'¥{self.amount}'
 
 class TestMoney(unittest.TestCase):
     def test_金額を表示する(self):
         千円 = Money(1000, 'JPY')
+        一万円 = Money(10000, 'JPY')
         self.assertEqual(str(千円), '¥1000')
+        self.assertEqual(str(一万円), '¥10000')
 
 unittest.main(argv=[''], verbosity=2, exit=False)
