0f096d0af5b21e08fbf94541601a0a72c89562ab
k2works <kakimomokkuri@gmail.com>
2022-02-02 15:28:51 +0900

    feat: フィボナッチ数計算

--- src/main_test.py
+++ src/main_test.py
@@ -22,7 +22,7 @@ doctest.testmod(verbose=True)
 
 class FibonacciTest(unittest.TestCase):
     def test_fibonacci(self):
-        cases = [[0, 0], [1, 1], [2, 1], [3, 2]]
+        cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
         for number, expected in cases:
             with self.subTest(number=number, expected=expected):
                 self.assertEqual(fib(number), expected)
@@ -31,7 +31,8 @@ class FibonacciTest(unittest.TestCase):
 def fib(n):
     if n == 0: return 0
     if n <= 2: return 1
-    return 2
+
+    return fib(n - 1) + fib(n - 2)
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
