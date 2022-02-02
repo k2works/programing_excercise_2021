1838047d8be351a8f87628fb647605ed00c1ff4b
k2works <kakimomokkuri@gmail.com>
2022-02-02 15:45:59 +0900

    refactor: メソッド名の変更

--- src/main_test.py
+++ src/main_test.py
@@ -28,17 +28,17 @@ class FibonacciTest(unittest.TestCase):
         cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
         for number, expected in cases:
             with self.subTest(number=number, expected=expected):
-                self.assertEqual(self.fib.fib(number), expected)
+                self.assertEqual(self.fib.calc(number), expected)
 
 
 class Fibonacci:
-    def fib(self, number):
+    def calc(self, number):
         if number == 0:
             return 0
         if number <= 2:
             return 1
 
-        return self.fib(number - 1) + self.fib(number - 2)
+        return self.calc(number - 1) + self.calc(number - 2)
 
 
 
