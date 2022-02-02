70cc5ff0d93fee2111da1e9d27fe4f64de0a2be9
k2works <kakimomokkuri@gmail.com>
2022-02-02 16:18:06 +0900

    refactor: メソッド名の変更

--- src/main_test.py
+++ src/main_test.py
@@ -29,20 +29,20 @@ class FibonacciTest(unittest.TestCase):
         cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
         for number, expected in cases:
             with self.subTest(number=number, expected=expected):
-                self.assertEqual(self.fib.calc(number), expected)
+                self.assertEqual(self.fib.recursive(number), expected)
 
-    def test_large_number(self):
-        self.assertEqual(self.fib.calc(40), 102334155)
+    def test_large_number_recursive(self):
+        self.assertEqual(self.fib.recursive(40), 102334155)
 
-    def test_large_number_calc2(self):
-        self.assertEqual(self.fib.calc2(40), 102334155)
+    def test_large_number_loop(self):
+        self.assertEqual(self.fib.loop(40), 102334155)
 
-    def test_large_number_calc3(self):
-        self.assertEqual(self.fib.calc3(40), 102334155)
+    def test_large_number_general_term(self):
+        self.assertEqual(self.fib.general_term(40), 102334155)
 
 
 class Fibonacci:
-    def calc(self, number, memo = {}):
+    def recursive(self, number, memo={}):
         if number == 0:
             return 0
         if number <= 2:
@@ -51,20 +51,21 @@ class Fibonacci:
         if number in memo:
             return memo[number]
 
-        memo[number] = self.calc(number - 1, memo) + self.calc(number - 2, memo)
+        memo[number] = self.recursive(
+            number - 1, memo) + self.recursive(number - 2, memo)
         return memo[number]
         
-    def calc2(self, number):
+    def loop(self, number):
         a = 0
         b = 1
         c = 0
-        for i in range(number):
+        for _ in range(number):
             a = b
             b = c
             c = a + b
         return c
 
-    def calc3(self, number):
+    def general_term(self, number):
         a = ((1 + math.sqrt(5)) / 2)**number
         b = ((1 - math.sqrt(5)) / 2)**number
         return round(((a - b) / math.sqrt(5)))
