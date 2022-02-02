72809a6d22d467d1b41894a3506a25a61b7cfbfd
k2works <kakimomokkuri@gmail.com>
2022-02-02 15:24:11 +0900

    test: 明白な実装

--- src/main_test.py
+++ src/main_test.py
@@ -1,5 +1,6 @@
 # %%
 # Doctest
+import numbers
 import unittest
 import doctest
 
@@ -21,15 +22,16 @@ doctest.testmod(verbose=True)
 
 class FibonacciTest(unittest.TestCase):
     def test_fibonacci(self):
-        cases = [[0, 0], [1, 1]]
-        for expected, actual in cases:
-            with self.subTest(expected=expected, actual=actual):
-                self.assertEqual(fib(expected), actual)
+        cases = [[0, 0], [1, 1], [2, 1], [3, 2]]
+        for number, expected in cases:
+            with self.subTest(number=number, expected=expected):
+                self.assertEqual(fib(number), expected)
 
 
 def fib(n):
     if n == 0: return 0
-    return 1
+    if n <= 2: return 1
+    return 2
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
