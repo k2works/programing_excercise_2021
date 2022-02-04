3b5d0d0157f0ee4605dcabb9be49ab6a1b27c688
k2works <kakimomokkuri@gmail.com>
2022-02-04 16:49:42 +0900

    refactor: メソッド名の変更

--- src/main_test.py
+++ src/main_test.py
@@ -12,19 +12,19 @@ class TestFibonacci(unittest.TestCase):
         cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
         for number, expected in cases:
             with self.subTest(number=number, expected=expected):
-                self.assertEqual(self.fib.calc(number), expected)
+                self.assertEqual(self.fib.recursive(number), expected)
 
-    def test_large_number(self):
-        self.assertEqual(self.fib.calc(40), 102_334_155)
+    def test_large_number_recursive(self):
+        self.assertEqual(self.fib.recursive(40), 102_334_155)
 
     def test_large_number_2(self):
-        self.assertEqual(self.fib.calc2(40), 102_334_155)
+        self.assertEqual(self.fib.loop(40), 102_334_155)
 
     def test_large_number_3(self):
-        self.assertEqual(self.fib.calc3(40), 102_334_155)
+        self.assertEqual(self.fib.general_term(40), 102_334_155)
 
 class Fibonacci:
-    def calc(self, number, memo={}):
+    def recursive(self, number, memo={}):
         if number == 0:
             return 0
         if number <= 2:
@@ -33,11 +33,11 @@ class Fibonacci:
         if number in memo:
             return memo[number]
 
-        memo[number] = self.calc(number - 1, memo) + \
-            self.calc(number - 2, memo)
+        memo[number] = self.recursive(number - 1, memo) + \
+            self.recursive(number - 2, memo)
         return memo[number]
 
-    def calc2(self, number):
+    def loop(self, number):
         a = 0
         b = 1
         c = 0
@@ -47,7 +47,7 @@ class Fibonacci:
             c = a + b
         return c
 
-    def calc3(self, number):
+    def general_term(self, number):
         a = ((1 + math.sqrt(5)) / 2) ** number
         b = ((1 - math.sqrt(5)) / 2) ** number
         return round( (a - b) / math.sqrt(5) )
