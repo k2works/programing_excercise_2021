127f9070fb30441a0be4e32deeebe3df1c2dac5c
k2works <kakimomokkuri@gmail.com>
2022-02-04 15:58:44 +0900

    feat: フィボナッチ数計算

--- src/main_test.py
+++ src/main_test.py
@@ -1,9 +1,11 @@
 import unittest
 
+from pkg_resources import find_distributions
+
 
 class TestFibonacci(unittest.TestCase):
     def test_fibonacci(self):
-        cases = [[0, 0], [1, 1], [2, 1],[3, 2]]
+        cases = [[0, 0], [1, 1], [2, 1],[3, 2], [4, 3], [5, 5]]
         for number, expected in cases:
             with self.subTest(number=number, expected=expected):
                 self.assertEqual(fib(number), expected)
@@ -15,7 +17,7 @@ def fib(n):
     if n <= 2:
         return 1
 
-    return 2
+    return fib(n - 1) + fib(n - 2)
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
