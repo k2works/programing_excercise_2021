6482a1a4ec5de2d6853b88ca57759dc435d0e607
k2works <kakimomokkuri@gmail.com>
2022-02-04 16:54:56 +0900

    refactor(WIP): サブクラスによるタイプコードの置き換え

--- src/main_test.py
+++ src/main_test.py
@@ -8,6 +8,7 @@ class TestFibonacci(unittest.TestCase):
     def setUp(self) -> None:
         self.fib = Fibonacci()
         self.recursive = FibonacciRecursive()
+        self.loop = FibonacciLoop()
 
     def test_fibonacci(self):
         cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
@@ -19,26 +20,18 @@ class TestFibonacci(unittest.TestCase):
         self.assertEqual(self.recursive.exec(40), 102_334_155)
 
     def test_large_number_2(self):
-        self.assertEqual(self.fib.loop(40), 102_334_155)
+        self.assertEqual(self.loop.exec(40), 102_334_155)
 
     def test_large_number_3(self):
         self.assertEqual(self.fib.general_term(40), 102_334_155)
 
-class Fibonacci:
-    def loop(self, number):
-        a = 0
-        b = 1
-        c = 0
-        for _ in range(number):
-            a = b
-            b = c
-            c = a + b
-        return c
 
+class Fibonacci:
     def general_term(self, number):
         a = ((1 + math.sqrt(5)) / 2) ** number
         b = ((1 - math.sqrt(5)) / 2) ** number
-        return round( (a - b) / math.sqrt(5) )
+        return round((a - b) / math.sqrt(5))
+
 
 class FibonacciRecursive:
     def exec(self, number, memo={}):
@@ -53,5 +46,18 @@ class FibonacciRecursive:
         memo[number] = self.exec(number - 1, memo) + \
             self.exec(number - 2, memo)
         return memo[number]
+
+
+class FibonacciLoop:
+    def exec(self, number):
+        a = 0
+        b = 1
+        c = 0
+        for _ in range(number):
+            a = b
+            b = c
+            c = a + b
+        return c
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
