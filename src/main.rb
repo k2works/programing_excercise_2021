25a3584c0bb5868b2821eb5e4890634ac8f71d59
k2works <kakimomokkuri@gmail.com>
2022-02-04 14:21:47 +0900

    test: 明白な実装

--- src/main.rb
+++ src/main.rb
@@ -7,12 +7,13 @@ require 'minitest/autorun'
 class FibonacciTest < Minitest::Test
   def fib(n)
     return 0 if n.zero?
+    return 1 if n <= 2
 
-    1
+    2
   end
 
   def test_fibonacci
-    cases = [[0,0], [1,1], [2,1]]
+    cases = [[0,0], [1,1], [2,1], [3,2]]
     cases.each do |i|
       assert_equal i[1], fib(i[0])
     end
