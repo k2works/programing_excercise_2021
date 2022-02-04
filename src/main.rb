6c1796ffecf48c0e3d6dba2019b1ef7bdd97a61e
k2works <kakimomokkuri@gmail.com>
2022-02-04 14:26:50 +0900

    feat: フィボナッチ数計算

--- src/main.rb
+++ src/main.rb
@@ -7,13 +7,13 @@ require 'minitest/autorun'
 class FibonacciTest < Minitest::Test
   def fib(n)
     return 0 if n.zero?
-    return 1 if n <= 2
+    return 1 if n == 1
 
-    2
+    fib(n - 1) + fib(n - 2)
   end
 
   def test_fibonacci
-    cases = [[0,0], [1,1], [2,1], [3,2]]
+    cases = [[0,0], [1,1], [2,1], [3,2], [4,3]]
     cases.each do |i|
       assert_equal i[1], fib(i[0])
     end
