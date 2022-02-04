4dc9eb9e91f1d0a672dfff3ac57adab643c6d35d
k2works <kakimomokkuri@gmail.com>
2022-02-04 14:29:06 +0900

    refactor: クラスの抽出

--- src/main.rb
+++ src/main.rb
@@ -5,17 +5,23 @@ Minitest::Reporters.use!
 require 'minitest/autorun'
 
 class FibonacciTest < Minitest::Test
-  def fib(n)
-    return 0 if n.zero?
-    return 1 if n == 1
-
-    fib(n - 1) + fib(n - 2)
+  def setup
+    @fib = Fibonacci
   end
 
   def test_fibonacci
     cases = [[0,0], [1,1], [2,1], [3,2], [4,3]]
     cases.each do |i|
-      assert_equal i[1], fib(i[0])
+      assert_equal i[1], @fib.fib(i[0])
     end
+  end
+end
+
+class Fibonacci
+  def self.fib(n)
+    return 0 if n.zero?
+    return 1 if n == 1
+
+    fib(n - 1) + fib(n - 2)
   end
 end
