466357225b01cc74b38a21570e380ce31ae633e9
k2works <kakimomokkuri@gmail.com>
2022-02-04 16:02:38 +0900

    refactor: クラスの抽出

--- src/main_test.py
+++ src/main_test.py
@@ -4,20 +4,24 @@ from pkg_resources import find_distributions
 
 
 class TestFibonacci(unittest.TestCase):
+    def setUp(self) -> None:
+        self.fib = Fibonacci()
+
     def test_fibonacci(self):
-        cases = [[0, 0], [1, 1], [2, 1],[3, 2], [4, 3], [5, 5]]
+        cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
         for number, expected in cases:
             with self.subTest(number=number, expected=expected):
-                self.assertEqual(fib(number), expected)
+                self.assertEqual(self.fib.fib(number), expected)
 
 
-def fib(n):
-    if n == 0:
-        return 0
-    if n <= 2:
-        return 1
+class Fibonacci:
+    def fib(self, n):
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
 
-    return fib(n - 1) + fib(n - 2)
+        return self.fib(n - 1) + self.fib(n - 2)
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
