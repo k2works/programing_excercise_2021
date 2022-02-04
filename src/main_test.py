b5ea4a7b046bcf8f76fab1a383c36d641cbd612d
k2works <kakimomokkuri@gmail.com>
2022-02-04 16:52:20 +0900

    refactor(WIP): サブクラスによるタイプコードの置き換え

--- src/main_test.py
+++ src/main_test.py
@@ -7,15 +7,16 @@ from pkg_resources import find_distributions
 class TestFibonacci(unittest.TestCase):
     def setUp(self) -> None:
         self.fib = Fibonacci()
+        self.recursive = FibonacciRecursive()
 
     def test_fibonacci(self):
         cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
         for number, expected in cases:
             with self.subTest(number=number, expected=expected):
-                self.assertEqual(self.fib.recursive(number), expected)
+                self.assertEqual(self.recursive.exec(number), expected)
 
     def test_large_number_recursive(self):
-        self.assertEqual(self.fib.recursive(40), 102_334_155)
+        self.assertEqual(self.recursive.exec(40), 102_334_155)
 
     def test_large_number_2(self):
         self.assertEqual(self.fib.loop(40), 102_334_155)
@@ -24,19 +25,6 @@ class TestFibonacci(unittest.TestCase):
         self.assertEqual(self.fib.general_term(40), 102_334_155)
 
 class Fibonacci:
-    def recursive(self, number, memo={}):
-        if number == 0:
-            return 0
-        if number <= 2:
-            return 1
-
-        if number in memo:
-            return memo[number]
-
-        memo[number] = self.recursive(number - 1, memo) + \
-            self.recursive(number - 2, memo)
-        return memo[number]
-
     def loop(self, number):
         a = 0
         b = 1
@@ -52,5 +40,18 @@ class Fibonacci:
         b = ((1 - math.sqrt(5)) / 2) ** number
         return round( (a - b) / math.sqrt(5) )
 
+class FibonacciRecursive:
+    def exec(self, number, memo={}):
+        if number == 0:
+            return 0
+        if number <= 2:
+            return 1
+
+        if number in memo:
+            return memo[number]
+
+        memo[number] = self.exec(number - 1, memo) + \
+            self.exec(number - 2, memo)
+        return memo[number]
 
 unittest.main(argv=[''], verbosity=2, exit=False)
