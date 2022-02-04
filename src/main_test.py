4c4652b8af22c6af9983617fdd8a378055ae1ec5
k2works <kakimomokkuri@gmail.com>
2022-02-04 15:51:14 +0900

    test: 三角測量

--- src/main_test.py
+++ src/main_test.py
@@ -4,10 +4,13 @@ import unittest
 class TestFibonacci(unittest.TestCase):
     def test_fibonacci(self):
         self.assertEquals(fib(0), 0)
+        self.assertEquals(fib(1), 1)
 
 
 def fib(n):
-    return 0
+    if n == 0:
+        return 0
+    return 1
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
