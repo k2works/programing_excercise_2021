159e6f854e7259cdac10e166ca12bc55b04e2304
k2works <kakimomokkuri@gmail.com>
2022-02-04 15:49:59 +0900

    test: 仮実装

--- src/main_test.py
+++ src/main_test.py
@@ -1,33 +1,13 @@
-# %%
-# Doctest
 import unittest
-import doctest
 
 
-def add(a, b):
-    """Return the sum of a and b.
-    >>> add(2, 2)
-    4
-    """
-    sum = a
-    return sum
+class TestFibonacci(unittest.TestCase):
+    def test_fibonacci(self):
+        self.assertEquals(fib(0), 0)
 
 
-doctest.testmod(verbose=True)
-
-# %%
-# Unittest
-
-
-class TestFunction(unittest.TestCase):
-    def test_add(self):
-        self.assertEquals(add(2, 2), 4)
-
-
-def add(a, b):
-    """Return the sum of a and b."""
-    sum = a + b
-    return sum
+def fib(n):
+    return 0
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
